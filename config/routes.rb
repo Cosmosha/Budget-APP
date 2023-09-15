569c489f4aefa29b511821704a08f00b91fb814d
cosmosha <cosmoshagan@gmail.com>
2023-09-15 07:27:41 +0000

    Add files for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'signup',
+    password: 'secret',
+    confirmation: 'verification',
+    unlock: 'unblock',
+  }
+
+  root to: 'splash#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :categories, only: [:index, :new, :create] do
+    resources :transactions, only: [:index, :new, :create]
+  end
+
 end
