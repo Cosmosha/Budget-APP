7b870e937b3b73de616b85090432ad3c9988f2c3
cosmosha <cosmoshagan@gmail.com>
2023-09-15 07:25:20 +0000

    Add controllers for users

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,21 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.includes(:transactions).where(author: current_user)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    current_user.categories.create(category_params)
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
+
+end
